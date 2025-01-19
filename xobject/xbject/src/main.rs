bbee5c88a220bc4d0e5ca895131fcdb0a20d22b9
hermann wenninger <info@wennhmann.de>
2025-01-19 17:14:13 +0100

    impl test to xobject

--- xobject/xbject/src/main.rs
+++ xobject/xbject/src/main.rs
@@ -1,13 +1,13 @@
 mod handler;
 
 
-use axum::response::IntoResponse;
+//use axum::response::IntoResponse;
 use tokio_postgres::{NoTls, Row};
 use tokio_postgres::Error;
 use axum::{ Router};
 use axum::routing::get;
 
-#[derive(Debug)]
+#[derive(Debug,PartialEq)]
 pub struct User {
   pub id: i32,
   pub email: String,
@@ -32,6 +32,18 @@ async fn main() -> Result<(), Error> {
  //   async fn get_users_handler() -> impl IntoResponse {
        
    // }
+  let users = get_users().await?;
+
+  println!("{:#?}", users);
+
+  axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+  Ok(())
+}
+
+async fn get_users() -> Result<Vec<User>, Error> {
   // Get DB client and connection
   let (client, connection) = tokio_postgres::connect(
     "postgres://postgres:23hermann75%40#*@127.0.0.1:5432/nails?sslmode=disable",
@@ -51,11 +63,40 @@ async fn main() -> Result<(), Error> {
 
   let users: Vec<User> = result.into_iter().map(|row| User::from(row)).collect();
 
-  println!("{:#?}", users);
-
-  axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-  Ok(())
+  Ok(users)
 }
+    #[tokio::test]
+    async fn test_success() {
+        let users = get_users().await.unwrap();
+        assert_eq!(users,[
+          User {
+              id: 1,
+              email: "test1@test1.com".to_string(),
+          },
+          User {
+              id: 2,
+              email: "test2@test1.com".to_string(),
+          },
+          User {
+              id: 3,
+              email: "test3@test1.com".to_string(),
+          },
+          User {
+              id: 4,
+              email: "test4@test1.com".to_string(),
+          },
+          User {
+              id: 5,
+              email: "test5@test1.com".to_string(),
+          },
+          User {
+              id: 6,
+              email: "test6@test1.com".to_string(),
+          },
+          User {
+              id: 7,
+              email: "test677@test1.com".to_string(),
+          },
+      ]);
+    }
+    
